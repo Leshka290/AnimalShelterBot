b5036c3b462e379ab2ba47572de51909dbd88a7b
youcanwakemeup <127168034+youcanwakemeup@users.noreply.github.com>
2023-08-02 19:51:38 +0400

    tests for service

--- src/main/java/com/skyteam/animalshelterbot/repository/DogClientRepository.java
+++ src/main/java/com/skyteam/animalshelterbot/repository/DogClientRepository.java
@@ -2,8 +2,9 @@ package com.skyteam.animalshelterbot.repository;
 
 import com.skyteam.animalshelterbot.model.DogClient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DogClientRepository extends JpaRepository<DogClient, Long> {
-   // boolean existsByChatId(long chatId);
 
 }
