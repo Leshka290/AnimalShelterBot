85ab5960512a9033b5ccb00bb7a229d6d169a0ab
leshka290 <lleesshhkkaa00@gmail.com>
2023-08-06 08:48:29 +0300

    change Pet

--- src/main/java/com/skyteam/animalshelterbot/model/Pet.java
+++ src/main/java/com/skyteam/animalshelterbot/model/Pet.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "pets")
 @Data
+@NoArgsConstructor
 public class Pet {
 
     @Id
@@ -17,10 +18,21 @@ public class Pet {
     private Long id;
     private String nickName;
     private PetType petType;
+    private String breed;
     private Sex sex;
+    private int age;
     private byte[] picture;
 
     @ManyToOne
     @JoinColumn(name = "adopter_id")
     private Adopter adopterId;
+
+    public Pet(String nickName, PetType petType, String breed, Sex sex, int age, byte[] picture) {
+        this.nickName = nickName;
+        this.petType = petType;
+        this.breed = breed;
+        this.sex = sex;
+        this.age = age;
+        this.picture = picture;
+    }
 }
--- src/main/java/com/skyteam/animalshelterbot/repository/VolunteerRepository.java
+++ src/main/java/com/skyteam/animalshelterbot/repository/VolunteerRepository.java
@@ -1,8 +1,10 @@
 package com.skyteam.animalshelterbot.repository;
 
 
-import com.skyteam.animalshelterbot.model.AdopterVolunteer;
+import com.skyteam.animalshelterbot.model.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface VolunteerRepository extends JpaRepository<AdopterVolunteer, Long> {
+@Repository
+public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
 }
