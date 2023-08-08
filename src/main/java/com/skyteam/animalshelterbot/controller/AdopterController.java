325f7599b667e4736050c30bc92eafb6e128820a
Brein <Brein_02_93@mail.ru>
2023-08-08 21:05:28 +0300

    Добавил описание API

--- src/main/java/com/skyteam/animalshelterbot/controller/AdopterController.java
+++ src/main/java/com/skyteam/animalshelterbot/controller/AdopterController.java
@@ -3,11 +3,16 @@ package com.skyteam.animalshelterbot.controller;
 import com.skyteam.animalshelterbot.model.Adopter;
 import com.skyteam.animalshelterbot.repository.AdopterRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Tag(name = "API для работы с усыновителями", description = "Содержит методы для создания усыновителей")
 @RestController
 public class AdopterController {
     private final AdopterRepository adopterRepository;
@@ -17,7 +22,17 @@ public class AdopterController {
     }
 
     @PostMapping
-    @Operation(summary = "Добавить нового владельца животного в БД")
+    @Operation(summary = "Добавить нового владельца животного в БД",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Владелец добавлен в БД",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Adopter.class)
+                            )
+                    )
+            })
     public Adopter createAdopter(@RequestBody Adopter adopter) {
         return adopterRepository.save(adopter);
     }
